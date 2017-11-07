df8e15620032a82fe952f58b73e1ab49622c4e0a
eddycheong <cheong.e49@gmail.com>
2017-11-07 21:34:08 +0000

    Updated store and employee model. Finish exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Eddy", last_name: "Cheong", hourly_rate: 80)
+@store1.employees.create(first_name: "Jon", last_name: "Gasper", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Jimmy", last_name: "Raynor", hourly_rate: 60)
